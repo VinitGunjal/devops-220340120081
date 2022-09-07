138b92cd2d987970037c7886b560f2f5c35d53da
VinitGunjal <vinitgunjal1999@gmai.com>
2022-09-07 13:03:27 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,7 @@
+class Addition{
+                 public static void main(String[] args){
+                      int num1=11;
+                      int num2=22;
+                     System.out.println("Sum"+(num1+num2));
+                 }
+}
